1e3e9c60de2b6234c5d5dda01ef05acb813315e5
Ayan Khurt <ayankhurt@gmail.com>
2025-07-15 03:41:27 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 // import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
-// import Navbar from './componement/Navbar'
-// import Main from './componement/main'
-import NotFound from './componement/NotFound'
+import Navbar from './componement/Navbar';
+import Main from './componement/main';
+// import NotFound from './componement/NotFound'
 
 function App() {
     return (
         <>
-            <Routes>
-                <Route path="*" element={<NotFound />} /> {/* This is the 404 route */}
-            </Routes>
+        <Navbar />
+        <Main />
         </>
     );
 }
